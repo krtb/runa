df85aa895457d648f2576202f36393ec1b78f567
Kurt Bauer <kurtbauer55@protonmail.com>
2021-07-07 19:58:30 -0400

    pass client apollo prop to ApolloProvider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,16 +4,15 @@ import Page from '../components/Page';
 import '../components/styles/nprogress.css';
 import { ApolloProvider } from '@apollo/client';
 import withData from '../lib/withData';
-import { loadGetInitialProps } from 'next/dist/next-server/lib/utils';
-import { Component } from 'react';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({Component, pageProps, apollo}) {
+	console.log(apollo);
 	return (
-		<ApolloProvider>
+		<ApolloProvider client={apollo} >
 			<Page>
 				<Component {...pageProps}/>
 			</Page>
@@ -29,7 +28,7 @@ MyApp.getInitialProps = async function({Component, ctx}){
 	}
 	// make props available at a page level
 	pageProps.query = ctx.query
-	return pageProps;
+	return {pageProps};
 }
 // send App, but inject apollo client alongside, with help of Provider function
 export default withData(MyApp);
