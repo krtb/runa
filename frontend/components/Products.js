3d37648afb171b25cab805e1729866ddbfdc53c6
Kurt Bauer <kurtbauer55@protonmail.com>
2021-07-09 09:52:08 -0400

    add graphql methods for loading,errors. Add styled component

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,5 +1,6 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
 
 const ALL_PRODUCTS_QUERY = gql`
 query ALL_PRODUCTS_QUERY {
@@ -18,12 +19,27 @@ query ALL_PRODUCTS_QUERY {
 }
 `
 
+const ProductsListsStyles = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-gap: 60px;
+`;
+
 export default function Products(){
 	const {data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
 	
+	// can replace with a Loading component in the future
+	if(loading) return <p>Loading...</p>;
+	if(error) return <p> Error: {error.message} </p>;
+
 	return(
 		<div>
-			<p>products?</p>
+			<ProductsListsStyles>
+				{data.allProducts.map(product => (
+					<p key={product.id}> {product.name} </p>
+					))
+				}
+			</ProductsListsStyles>
 		</div>
 	);
 }
