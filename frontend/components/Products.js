3c383578bef734675456a4dcaa693a9265ff8a30
Kurt Bauer <kurtbauer55@protonmail.com>
2021-07-09 09:17:14 -0400

    import Products component

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,7 +1,29 @@
-import Products from "./Products";
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const ALL_PRODUCTS_QUERY = gql`
+query ALL_PRODUCTS_QUERY {
+  allProducts{
+    id
+    name
+    price
+    description
+    photo {
+      id
+      image {
+        publicUrlTransformed
+      }
+    }
+  }
+}
+`
 
 export default function Products(){
+	const {data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+	
 	return(
-		<Products/>
+		<div>
+			<p>products?</p>
+		</div>
 	);
 }
