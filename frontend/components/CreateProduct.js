091277648cf544fbdad100e2f3be8b90adb2483c
Kurt Bauer <kurtbauer55@protonmail.com>
2021-07-26 19:07:47 -0400

    use React Hook, setName, to use and update input state

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,11 +1,27 @@
 // import React from 'react' => Don't need to import React anymore, it's assumed
 
+import { useState } from "react"
+
+
 export default function CreateProduct() {
+
+	// Reactive Piece of state => whenever this piece of state changes, it will be updated in the app.
+	const [name, setName] = useState('Kurt')
+
 	return (
 		<form>
 			<label htmlFor="name">
 				Name
-				<input type="text" id="name" name="name" placeholder="Name" />				
+				<input 
+					type="text" 
+					id="name" 
+					name="name" 
+					placeholder="Name"
+					value={name}
+					onChange={e => {
+						setName(e.target.value)
+					}}
+				/>				
 			</label>
 		</form>
 	)
