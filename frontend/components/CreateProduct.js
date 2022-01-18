415d92c1a3177e441f83eddfd467ae50f4ae855e
Kurt Bauer <kurtbauer55@protonmail.com>
2022-01-17 22:23:09 -0500

    change quotes from double to single

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,25 +1,25 @@
-// import { useState } from 'react'; => using custom useForm webhook, instead of useState
-import useForm from '../lib/useForm';
-import Form from './styles/Form'
+// import { useState } from "react"; => using custom useForm webhook, instead of useState
+import useForm from "../lib/useForm";
+import Form from "./styles/Form"
 
 export default function createProduct(){
 
 	// destructuring inputs
 	const {inputs, handleChange, clearForm, resetForm} = useForm({
-		name: 'bag',
+		name: "bag",
 		price: 100,
-		description: 'this is a sweet bag!'
+		description: "this is a sweet bag!"
 	});
 
 	return(
 		<Form>
-			<label htmlFor='name'>
+			<label htmlFor="name">
 				Name 
 				<input
-					type='text'
-					id='name'
-					name='name'
-					placeholder='Name'
+					type="text"
+					id="name"
+					name="name"
+					placeholder="Name"
 					value={inputs.name}
 					onChange={handleChange}
 				/>
@@ -27,25 +27,29 @@ export default function createProduct(){
 				
 			</label>
 
-			<label htmlFor='price'>
+			<label htmlFor="price">
 				Price 
 				<input
-					type='number'
-					id='price'
-					name='price'
-					placeholder='price'
+					type="number"
+					id="price"
+					name="price"
+					placeholder="price"
 					value={inputs.price}
 					onChange={handleChange}
 				/>
 			</label>
 
-			<button type='button' onClick={clearForm}>
+			<button type="button" >
+				+ Add Product
+			</button>
+
+			{/* <button type="button" onClick={clearForm}>
 				Clear Form
 			</button>
 
-			<button type='button' onClick={resetForm}>
+			<button type="button" onClick={resetForm}>
 				Reset Form
-			</button>
+			</button> */}
 		</Form>
-	)                           
+	)                          
 }
